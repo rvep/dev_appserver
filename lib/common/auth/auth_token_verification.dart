2ebdb0bd6c51b5b29c0b7c10a30d986039b3b51f
borysn <xborysn@gmail.com>
2016-08-21 14:37:23 -0500

    update library definition

--- lib/common/auth/auth_token_verification.dart
+++ lib/common/auth/auth_token_verification.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2016, borysn. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-library dev_appserver.lib.common.auth.auth_token;
+library dev_appserver.lib.common.auth.auth_token_verification;
 
 class AuthTokenVerification {
   bool _isVerified;
