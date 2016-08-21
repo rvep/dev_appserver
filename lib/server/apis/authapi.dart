a86904fefad1ab55d7abe2595c5737ce9c464905
borysn <xborysn@gmail.com>
2016-08-21 15:22:55 -0500

    init firebase

--- lib/server/apis/authapi.dart
+++ lib/server/apis/authapi.dart
@@ -4,6 +4,7 @@
 library dev_appserver.lib.server.api.authapi;
 
 import 'package:rpc/rpc.dart';
+import 'package:firebase3/firebase.dart' as firebase;
 import 'package:dev_appserver/common/auth/auth_token.dart';
 import 'package:dev_appserver/common/auth/auth_token_verification.dart';
 import 'package:dev_appserver/server/services/google_auth_verifier.dart';
@@ -11,6 +12,15 @@ import 'package:dev_appserver/server/services/google_auth_verifier.dart';
 @ApiClass(version: 'v1')
 class AuthApi {
   GoogleAuthVerifier glAuthVerifier;
+
+  AuthApi() {
+    // init firebase
+    firebase.initializeApp(
+      apiKey: "AIzaSyC1aHWikGh18FBBwuVbSGuUu1lQvWCOUnY",
+      authDomain: "rvep-1212.firebaseapp.com",
+      databaseURL: "https://rvep-1212.firebaseio.com",
+      storageBucket: "rvep-1212.appspot.com");
+  }
 
   @ApiMethod(method: 'GET', path: 'google')
   AuthTokenVerification verifyGoogleAuth(AuthToken authToken) {
