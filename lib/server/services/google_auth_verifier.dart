7861de93fcf8ab718cde4895a73a9e29a1e67dd4
borysn <xborysn@gmail.com>
2016-08-21 15:35:50 -0500

    not sure how to verify tokens using firebase

--- lib/server/services/google_auth_verifier.dart
+++ lib/server/services/google_auth_verifier.dart
@@ -3,13 +3,13 @@
 
 library dev_appserver.lib.server.services.google_auth_verifier;
 
-import 'package:googleapis/oauth2/v2.dart';
+import 'package:firebase3/firebase.dart' as firebase;
 import 'package:dev_appserver/common/auth/auth_token.dart';
 
 class GoogleAuthVerifier {
 
   bool verify(AuthToken authToken) {
-    return false;
+    firebase.auth().currentUser.getToken();
   }
 
 }
