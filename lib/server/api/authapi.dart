d60a5d9155c6c8eb10ccf2fa1037e7288ae8f96f
borysn <xborysn@gmail.com>
2016-08-21 14:43:54 -0500

    update library definition, add GoogleAuthVerifier service

--- lib/server/api/authapi.dart
+++ lib/server/api/authapi.dart
@@ -1,18 +1,20 @@
 // Copyright (c) 2016, borysn. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-library dev_appserver.lib.server.authapi;
+library dev_appserver.lib.server.api.authapi;
 
 import 'package:rpc/rpc.dart';
 import 'package:dev_appserver/common/auth/auth_token.dart';
 import 'package:dev_appserver/common/auth/auth_token_verification.dart';
+import 'package:dev_appserver/server/services/google_auth_verifier.dart';
 
 @ApiClass(version: 'v1')
 class AuthApi {
+  GoogleAuthVerifier glAuthVerifier;
 
   @ApiMethod(path: 'google')
   AuthTokenVerification verifyGoogleAuth(AuthToken authToken) {
-    
+
   }
 
 }
