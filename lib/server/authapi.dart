a2864d5781bf0eb0249c627f6bbcabb4ae73ae7b
borysn <xborysn@gmail.com>
2016-08-21 14:38:50 -0500

    add library definition

--- lib/server/authapi.dart
+++ lib/server/authapi.dart
@@ -1,6 +1,8 @@
 // Copyright (c) 2016, borysn. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+library dev_appserver.lib.server.authapi;
+
 import 'package:rpc/rpc.dart';
 import 'package:dev_appserver/common/auth/auth_token.dart';
 import 'package:dev_appserver/common/auth/auth_token_verification.dart';
@@ -10,7 +12,7 @@ class AuthApi {
 
   @ApiMethod(path: 'google')
   AuthTokenVerification verifyGoogleAuth(AuthToken authToken) {
-    
+
   }
 
 }
