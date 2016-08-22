2625f87f9ff0fe285f3d881bbc79419eb4a2b77f
borysn <xborysn@gmail.com>
2016-08-21 22:49:34 -0500

    update library definitions

--- lib/common/auth/auth_token.dart
+++ lib/common/auth/auth_token.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2016, borysn. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-library dev_appserver.lib.common.auth.auth_token;
+library common.auth.auth_token;
 
 class AuthToken {
   String _tokenId;
