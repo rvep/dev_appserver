ed4d34fd16e38d4192293e4c2eaab259f91cd2d7
borysn <xborysn@gmail.com>
2016-08-21 23:55:05 -0500

    adding shelf_rpc, but doesnt fix rpc:generate

--- bin/server.dart
+++ bin/server.dart
@@ -1,32 +1,21 @@
 // Copyright (c) 2016, borysn. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'dart:io';
+library server;
 
-import 'package:args/args.dart';
-import 'package:shelf/shelf.dart' as shelf;
+import 'package:rpc/rpc.dart';
 import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_rpc/shelf_rpc.dart' as shelf_rpc;
+import 'package:dev_appserver/server/apis/authapi.dart';
 
-void main(List<String> args) {
-  var parser = new ArgParser()
-    ..addOption('port', abbr: 'p', defaultsTo: '8088');
+void main() {
+  // Create your RPC API Server.
+  ApiServer apiServer = new ApiServer(prettyPrint: true);
+  apiServer.addApi(new AuthApi());
 
-  var result = parser.parse(args);
+  // Create a Shelf handler for your RPC API.
+  var handler = shelf_rpc.createRpcHandler(apiServer);
 
-  var port = int.parse(result['port'], onError: (val) {
-    stdout.writeln('Could not parse port value "$val" into a number.');
-    exit(1);
-  });
-
-  var handler = const shelf.Pipeline()
-      .addMiddleware(shelf.logRequests())
-      .addHandler(_echoRequest);
-
-  io.serve(handler, '0.0.0.0', port).then((server) {
-    print('Serving at http://${server.address.host}:${server.port}');
-  });
-}
-
-shelf.Response _echoRequest(shelf.Request request) {
-  return new shelf.Response.ok('Request for "${request.url}"');
+  // Start serving with Shelf.
+  io.serve(handler, 'localhost', 8088);
 }
