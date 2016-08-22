bdbd30b9b924b3b495b93f970c6f01ddd8f88333
borysn <xborysn@gmail.com>
2016-08-22 14:33:08 -0500

    playing around

--- bin/server.dart
+++ bin/server.dart
@@ -3,19 +3,43 @@
 
 library server;
 
+import 'dart:async';
+import 'dart:io';
+import 'package:logging/logging.dart';
 import 'package:rpc/rpc.dart';
-import 'package:shelf/shelf_io.dart' as io;
-import 'package:shelf_rpc/shelf_rpc.dart' as shelf_rpc;
 import 'package:dev_appserver/server/apis/authapi.dart';
 
-void main() {
-  // Create your RPC API Server.
-  ApiServer apiServer = new ApiServer(prettyPrint: true);
-  apiServer.addApi(new AuthApi());
+final ApiServer _apiServer = new ApiServer(prettyPrint: true);
 
-  // Create a Shelf handler for your RPC API.
-  var handler = shelf_rpc.createRpcHandler(apiServer);
+main() async {
+  // Add a bit of logging...
+  Logger.root..level = Level.INFO
+    ..onRecord.listen(print);
 
-  // Start serving with Shelf.
-  io.serve(handler, 'localhost', 8088);
+  // Set up a server serving the pirate API.
+  _apiServer.addApi(new AuthApi());
+  HttpServer server =
+  await HttpServer.bind(InternetAddress.ANY_IP_V4, 8088);
+  server.listen(requestHandler);
+  print('Server listening on http://${server.address.host}:'
+    '${server.port}');
+}
+
+Future requestHandler(HttpRequest request) async {
+  if (request.uri.path.startsWith('/authApi')) {
+    // Handle the API request.
+    var apiResponse;
+    try {
+      var apiRequest = new HttpApiRequest.fromHttpRequest(request);
+      apiResponse =
+      await _apiServer.handleHttpApiRequest(apiRequest);
+    } catch (error, stack) {
+      var exception =
+      error is Error ? new Exception(error.toString()) : error;
+      apiResponse = new HttpApiResponse.error(
+        HttpStatus.INTERNAL_SERVER_ERROR, exception.toString(),
+        exception, stack);
+    }
+    return sendApiResponse(apiResponse, request.response);
+  }
 }
