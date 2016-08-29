86ac83a67bd9f3c85cc44a250f0fcf6b7a1a6db2
borysn <xborysn@gmail.com>
2016-08-28 20:56:27 -0500

    uncomment broken code

--- lib/server/authverifyapi.dart
+++ lib/server/authverifyapi.dart
@@ -24,20 +24,20 @@ class AuthVerifyApi {
   AuthVerifyApi();
 
   @ApiMethod(method: 'POST', path: 'google')
-  AuthVerifyResponse verifyGoogleAuth(AuthVerifyRequest request) {
+    AuthVerifyResponse verifyGoogleAuth(AuthVerifyRequest request) {
     AuthVerifyResponse verification = new AuthVerifyResponse();
 
-   /* JsonWebToken jwt = new JsonWebToken.decode(request.token);
+    JsonWebToken jwt = new JsonWebToken.decode(request.token);
     JwtValidationContext jvc = new JwtValidationContext.withSharedSecret("AIzaSyC1aHWikGh18FBBwuVbSGuUu1lQvWCOUnY");
 
     try {
-      Set<ConstraintViolation> violations = jwt.validate(jvc);
+      jwt.validate(jvc);
       verification.isVerified = true;
       return verification;
     } catch (violation) {
       // claims may have been tampered with
       return verification;
-    }*/
+    }
 
     return verification;
   }
